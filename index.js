4a0cecd1082e3b0edb5610f9a3a54ef6ea57382a
Gbenga Oluwadahunsi <gbengaoluwadahunsi01@gmail.com>
2024-08-07 18:39:34 +0800

    Edit mongoose logic

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ app.use("/todos", postRoutes);
 //conect to mongoDb databasee
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on PORT  ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL)
+.then(() =>
+  app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+)
+.catch((error) => console.log(error.message));
+
+
+ 
